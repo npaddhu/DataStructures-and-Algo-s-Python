e3d614edb6aed42dc981f9c5d69ffa70318c9fba
npaddhu <npaddhu@gmail.com>
2019-05-13 20:10:38 +0530

    Adding chapter1-R1.1 solution

---
+++ chapter-01/1-reinforcement/1.py
@@ -0,0 +1,12 @@
+'''
+R-1.1 Write a short Python function, is_multiple(n, m), that takes two integer values and returns True if n is a multiple of m, that is,n = mi for some integer i, and False otherwise.
+'''
+
+def is_multiple(n, m):
+	return True if n%m == 0 else False
+
+if __name__=="__main__":
+	n = int(input('Enter n value'))
+	m = int(input('Enter m value'))
+	result = is_multiple(n, m)
+	print('n is multiple of m:', result)
